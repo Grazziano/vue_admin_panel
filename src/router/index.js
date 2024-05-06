c4a77c171741c9fcfaf40506605c892eedb34af5
Grazziano Borges Fagundes <grazzianofagundes@gmail.com>
2024-05-06 10:15:09 -0300

    feat: handle unauthorized users to redirect to login page

--- src/router/index.js
+++ src/router/index.js
@@ -5,27 +5,38 @@ import Login from '../views/Login.vue';
 import RequestPassword from '../views/RequestPassword.vue';
 import ResetPassword from '../views/ResetPassword.vue';
 import AppLayout from '../components/AppLayout.vue';
+import store from '../store';
 
 const routes = [
   {
     path: '/app',
     name: 'app',
     component: AppLayout,
+    meta: {
+      requiresAuth: true,
+    },
     children: [
       { path: 'dashboard', name: 'app.dashboard', component: Dashboard },
       { path: 'products', name: 'app.products', component: Products },
     ],
   },
 
-  { path: '/login', name: 'login', component: Login },
+  {
+    path: '/login',
+    name: 'login',
+    meta: { requiresGuest: true },
+    component: Login,
+  },
   {
     path: '/request-password',
     name: 'requestPassword',
+    meta: { requiresGuest: true },
     component: RequestPassword,
   },
   {
     path: '/reset-password/:token',
     name: 'resetPassword',
+    meta: { requiresGuest: true },
     component: ResetPassword,
   },
 ];
@@ -33,6 +44,17 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(),
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.requiresAuth && !store.state.user.token) {
+    next({ name: 'login' });
+    return;
+  } else if (to.meta.requiresGuest && store.state.user.token) {
+    next({ name: 'app.dashboard' });
+  } else {
+    next();
+  }
 });
 
 export default router;
