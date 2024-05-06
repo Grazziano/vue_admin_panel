ee035f58b752ba8d7df7f71eab2a60d7234bcc61
Grazziano Borges Fagundes <grazzianofagundes@gmail.com>
2024-05-05 22:29:30 -0300

    feat: create empty layout for authorized users

---
+++ src/components/AppLayout.vue
@@ -0,0 +1,27 @@
+<template lang="">
+  <div
+    class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8"
+  >
+    <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+      <h2
+        class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900"
+      >
+        {{ title }}
+      </h2>
+    </div>
+
+    <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+      <form class="space-y-6" action="#" method="POST">
+        <router-view></router-view>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const { title } = defineProps({
+  title: String,
+});
+</script>
+
+<style scoped></style>
